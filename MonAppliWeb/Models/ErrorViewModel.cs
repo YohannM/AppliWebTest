0c4604c8826bf8d9bd6b6de00c6320b4e0afb495
Yohann <yohannmarmonier@gmail.com>
2019-01-23 16:07:30 +0100

    premier commit

---
+++ MonAppliWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonAppliWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
